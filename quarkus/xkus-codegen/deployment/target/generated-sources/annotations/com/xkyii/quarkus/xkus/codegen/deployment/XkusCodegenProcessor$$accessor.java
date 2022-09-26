196007f03796a70e11c81612168ec16414d9267c
xkyii <xkyii@qq.com>
2022-09-26 11:43:03 +0800

    Up: Add dev-templates

--- quarkus/xkus-codegen/deployment/target/generated-sources/annotations/com/xkyii/quarkus/xkus/codegen/deployment/XkusCodegenProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package com.xkyii.quarkus.xkus.codegen.deployment;
-public final class XkusCodegenProcessor$$accessor {
-    private XkusCodegenProcessor$$accessor() {}
-    public static Object construct() {
-        return new XkusCodegenProcessor();
-    }
-}
