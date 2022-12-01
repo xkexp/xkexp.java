79d30736a413344dcd7390e7d86ce65151019b21
xkyii <xkyii@qq.com>
2022-12-01 19:39:02 +0800

    Up

---
+++ acm/src/main/java/huawei/hj043/Main.java
@@ -0,0 +1,29 @@
+package huawei.hj043;
+
+public class Main {
+    /**
+     * 迷宫问题
+     * TODO: 广度优先搜索
+     *
+     * input:
+5 5
+0 1 0 0 0
+0 1 1 1 0
+0 0 0 0 0
+0 1 1 1 0
+0 0 0 1 0
+     * output:
+(0,0)
+(1,0)
+(2,0)
+(2,1)
+(2,2)
+(2,3)
+(2,4)
+(3,4)
+(4,4)
+     */
+    public static void main(String[] args) {
+
+    }
+}
