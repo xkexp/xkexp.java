837dbb6b64c690bbb53f528b2a493c2e77821c8d
xkyii <xkyii@qq.com>
2022-12-07 17:50:56 +0800

    Up

--- acm/src/main/java/huawei/hj028/Main.java
+++ acm/src/main/java/huawei/hj028/Main.java
@@ -2,10 +2,13 @@ package huawei.hj028;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static final int INVALID = -1;
+
     /**
      * 匈牙利算法
      */
@@ -33,9 +36,11 @@ public class Main {
         int ret = 0;
         // 偶数的伴侣
         int[] evenMates = new int[evens.size()];
+        Arrays.fill(evenMates, INVALID);
         // 遍历计算
         for (int i = 0; i < odds.size(); i++) {
-            if (find(i, 0, odds, evens, evenMates)) {
+            boolean[] visit = new boolean[evens.size()];
+            if (find(i, odds, evens, evenMates, visit)) {
                 ret++;
             }
         }
@@ -46,25 +51,31 @@ public class Main {
     /**
      * 寻找奇数在偶数列表的伴侣
      * @param oddIndex 奇数序号
-     * @param evenIndex 偶数序号
      * @param evens 偶数列表
      * @param evenMates 偶数的伴侣
+     * @param visit 本轮偶数是否已经被匹配过
      * @return 是否找到伴侣
      */
-    private static boolean find(int oddIndex, int evenIndex, List<Integer> odds, List<Integer> evens, int[] evenMates) {
+    private static boolean find(int oddIndex, List<Integer> odds, List<Integer> evens, int[] evenMates, boolean[] visit) {
         Integer odd = odds.get(oddIndex);
-        for (int j = evenIndex; j < evens.size(); j++) {
+        for (int j = 0; j < evens.size(); j++) {
+            if (visit[j]) {
+                continue;
+            }
             Integer even = evens.get(j);
+//            System.out.println(String.format("odd: %d, even: %d", odd, even));
             // 可以成为伴侣
             if (isPrime(odd + even)) {
+                // 本轮搜索每个偶数只能匹配一次，且先到先得
+                visit[j] = true;
                 // 如果偶数还没有伴侣
                 Integer mate = evenMates[j];
-                if (mate == 0) {
+                if (mate == INVALID) {
                     evenMates[j] = oddIndex;
                     return true;
                 }
-                // 对方已经有伴侣,让他换一个,evenIndex及之前的都判断过了,从evenIndex开始
-                if (find(mate, evenIndex+1, odds, evens, evenMates)) {
+                // 对方已经有伴侣,让他换一个
+                if (find(mate, odds, evens, evenMates, visit)) {
                     evenMates[j] = oddIndex;
                     return true;
                 }
@@ -89,6 +100,7 @@ public class Main {
 
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
+//                System.out.printf("isPrime false: %d %d%n", n, i);
                 return false;
             }
         }
